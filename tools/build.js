17bd2eda04d2d694ef6d3e3941dcf3699678e016
deffer <irina.benediktovich@gmail.com>
2014-07-30 23:42:11 +1200

    Change build config to different locations

---
+++ tools/build.js
@@ -0,0 +1,14 @@
+({
+    "appDir": "../src/main/resources/META-INF/resources",
+    "baseUrl": "libs",
+    "paths": {
+        "app": "../main"
+    },
+    "dir": "../target/resources-built",
+	mainConfigFile: '../src/main/resources/META-INF/resources/main.js',
+    "modules": [
+        {
+            "name": "app"
+        }
+    ]
+})
