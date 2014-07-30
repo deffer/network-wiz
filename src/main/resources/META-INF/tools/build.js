df7fc4456709301acadca95883251c38473c49d9
Irina Benediktovich <i.benediktovich@auckland.ac.nz>
2014-07-30 18:00:11 +1200

    added config for optimization (only works with node.js yet)

---
+++ src/main/resources/META-INF/tools/build.js
@@ -0,0 +1,14 @@
+({
+    "appDir": "../resources",
+    "baseUrl": "libs",
+    "paths": {
+        "app": "../main"
+    },
+    "dir": "../resources-built",
+	mainConfigFile: '../resources/main.js',
+    "modules": [
+        {
+            "name": "app"
+        }
+    ]
+})
