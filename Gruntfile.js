84b85888e5495b81bed34ec0b0eccd1fd2da118c
deffer <irina.benediktovich@gmail.com>
2014-08-04 23:30:57 +1200

    Making usemingPrepare work

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,39 @@
 module.exports = function (grunt) {
-
+	require('load-grunt-tasks')(grunt);
+	
 	grunt.initConfig({
 
-		/*useminPrepare: {
-			html: 'index.html'
-		},*/
+		useminPrepare: {
+			html: 'scripts/index.html',
 
-		concat: {
-			generated: {
-				files: [
-					{ dest: 'dist/third-party.js',
-						src: [
-							'scripts/libs/underscore-min.js', 'scripts/libs/jquery.min.js',
-							'scripts/libs/angular.min.js', 'scripts/libs/arbor.js', 'scripts/libs/cytoscape.js'
-						]
-					},
-					{ dest: 'dist/app.js',
-						src: [
-							'scripts/js/my.js', 'scripts/js/myDataSource.js',
-							'scripts/js/graphManipulationService.js', 'scripts/js/dataManipulationService.js', 'scripts/js/layoutsFactory.js'
-						]
+			options: {
+				dest: 'dist',
+				staging: 'dist',
+				flow: {
+					html: {
+						steps: {
+							js: ['concat']
+						},
+						post: []
 					}
-				]
+				}
+			}
+		},
+
+		usemin: {
+			html: 'dist/index.html',
+			options: {
+				assetsDirs: ['dist']
 			}
 		}
-
 	});
 
 	grunt.loadNpmTasks('grunt-usemin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 
 	grunt.registerTask('build', [
 		'useminPrepare',
 		'concat:generated',
-		'filerev',
 		'usemin'
 	]);
 
