3c4573a22ea6cb3662844d4aeefe5352cc36da7c
deffer <irina.benediktovich@gmail.com>
2014-08-04 18:20:11 +1200

    Tryng Grunt with usemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,39 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  /*grunt.initConfig({
-  concat: {
-    options: {
-      separator: ';',
-    },
-    dist: {
-      src: ['scripts/libs/underscore-min.js', 'scripts/libs/jquery.min.js', 
-	  'scripts/libs/angular.min.js', 'scripts/libs/arbor.js', 'scripts/libs/cytoscape.js'],
-      dest: 'dist/third-party.js',
-    },
-  },
-  });
+	grunt.initConfig({
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  
-  // Default task(s).
-  grunt.registerTask('default', ['concat']);*/
-  
+		/*useminPrepare: {
+			html: 'index.html'
+		},*/
 
-  concat:
-    generated: {
-      files: [
-        {
-          dest: '.tmp/concat/js/app.js',
-          src: [
-            'app/js/app.js',
-            'app/js/controllers/thing-controller.js',
-            'app/js/models/thing-model.js',
-            'app/js/views/thing-view.js'
-          ]
-        }
-      ]
-    }
-  },
-  uglify: {
-    generated: {
-      files: [
-        {
-          dest: 'dist/js/app.js',
-          src: [ '.tmp/concat/js/app.js' ]
-        }
-      ]
-    }
-  }
+		concat: {
+			generated: {
+				files: [
+					{ dest: 'dist/third-party.js',
+						src: [
+							'scripts/libs/underscore-min.js', 'scripts/libs/jquery.min.js',
+							'scripts/libs/angular.min.js', 'scripts/libs/arbor.js', 'scripts/libs/cytoscape.js'
+						]
+					},
+					{ dest: 'dist/app.js',
+						src: [
+							'scripts/js/my.js', 'scripts/js/myDataSource.js',
+							'scripts/js/graphManipulationService.js', 'scripts/js/dataManipulationService.js', 'scripts/js/layoutsFactory.js'
+						]
+					}
+				]
+			}
+		}
 
-  grunt.registerTask('build', [
-  'useminPrepare',
-  'concat:generated',
-  'cssmin:generated',
-  'uglify:generated',
-  'filerev',
-  'usemin'
-]);
+	});
+
+	grunt.loadNpmTasks('grunt-usemin');
+
+	grunt.registerTask('build', [
+		'useminPrepare',
+		'concat:generated',
+		'filerev',
+		'usemin'
+	]);
 
 };
